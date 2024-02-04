59a9a9a3f140185881c9f95ebf1c37b1d46ead27
Samruddhi Kashmire <104366974+samruddhikashmire@users.noreply.github.com>
2024-02-04 22:48:39 +0530

    Create Day39.cpp
    80. Remove Duplicates from Sorted Array II

---
+++ Day39.cpp
@@ -0,0 +1,16 @@
+//80. Remove Duplicates from Sorted Array II
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        for(auto ele : nums)
+        {
+            if(i==0 || i==1 || nums[i-2]!=ele)
+            {
+                nums[i]=ele;
+                i++;
+            }
+        }
+        return i;
+    }
+};
