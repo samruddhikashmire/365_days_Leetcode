32fc7eac1ce88aea96be4fd807728e1824fc83c0
Samruddhi Kashmire <104366974+samruddhikashmire@users.noreply.github.com>
2024-02-25 23:03:48 +0530

    Create Day58.cpp
    9

---
+++ Day58.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int n) {
+    int last_digit = 0;
+    long int  rev = 0;
+    int x=n;
+    while(x>0)
+    {
+        last_digit = x % 10;
+        rev = (rev * 10 ) + last_digit;
+        x = x/10;
+        
+    }
+    if(n==rev)
+    return true;
+    else
+    return false;
+    }
+};
