2d938a9f3cca8940bc56a89dac1af4291f2fe98f
Samruddhi Kashmire <104366974+samruddhikashmire@users.noreply.github.com>
2024-02-29 23:19:26 +0530

    Update Day62.cpp
    875. Koko Eating Bananas

--- Day62.cpp
+++ Day62.cpp
@@ -22,3 +22,44 @@ public:
 
     }
 };
+
+
+// 875. Koko Eating Bananas
+class Solution {
+public:
+int findMax(vector<int> &v) {
+    int maxi = INT_MIN;
+    int n = v.size();
+    //find the maximum:
+    for (int i = 0; i < n; i++) {
+        maxi = max(maxi, v[i]);
+    }
+    return maxi;
+}
+int check(int mid , vector<int>& p , int h)
+{
+     int ans =0;
+     for(int i=0;i<p.size();i++)
+     {
+         ans += ceil((double)(p[i]) / (double)(mid));
+         if(ans > h)
+         break;
+     }
+     return ans;
+}
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+        int l = 1 ;
+        int high = findMax(piles);
+        while(l<=high)
+        {
+            int mid = (l+high)/2;
+            int val = check(mid , piles , h);
+            if(val <= h)
+              high = mid - 1;
+            else
+              l = mid + 1 ;
+        }
+        return l;
+    }
+};
