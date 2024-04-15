47e7271805ade0b13e05660ec3629e59730b72e2
Samruddhi Kashmire <104366974+samruddhikashmire@users.noreply.github.com>
2024-04-15 10:33:41 +0530

    Create Day103.cpp
    129. Sum Root to Leaf Numbers

---
+++ Day103.cpp
@@ -0,0 +1,20 @@
+// 129. Sum Root to Leaf Numbers
+class Solution {
+public: 
+    int solve(TreeNode *root , int curr)
+    {
+        if(!root)
+           return 0;
+
+        curr = (curr*10) + root->val;
+        if(root->left == NULL && root->right==NULL) 
+           return curr;
+
+        int l = solve(root->left , curr);
+        int r = solve(root->right , curr);
+        return (l+r);
+    }
+    int sumNumbers(TreeNode* root) {
+       return solve(root , 0);
+    }
+};
